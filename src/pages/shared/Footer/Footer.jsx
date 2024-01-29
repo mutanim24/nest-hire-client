43cd50e7bd132b738e39357ed0a026f67049c44c
mutanim24 <mutanim24@gmail.com>
2024-01-30 01:22:24 +0600

    added fotter

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            footer
+        <div className='p-5 bg-blue-200 text-center'>
+            <h4 className='font-semibold'>All Right Reserved By @ 2025</h4>
         </div>
     );
 };
